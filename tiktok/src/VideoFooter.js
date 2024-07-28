0cab03964c68972629fb19ccad9205c2070f9496
D-Limiter <>
2024-07-29 00:04:14 +0800

    Add account description

--- tiktok/src/VideoFooter.js
+++ tiktok/src/VideoFooter.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./VideoFooter.css";
+import AudiotrackIcon from "@mui/icons-material/Audiotrack";
+
 function VideoFooter() {
-  return <div className="videoFooter"></div>;
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3>@john_doe</h3>
+        <p>This is some description</p>
+        <div className="videoFooter__ticker">
+          <AudiotrackIcon className="videoFooter__icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default VideoFooter;
