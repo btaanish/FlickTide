ce7a098c445b5f0c6e2893478923b1f84e384f44
D-Limiter <>
2024-07-28 15:49:34 +0800

    Setup UI outline

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 import "./Video.css";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
   return (
     <div className="video">
-      <video 
-      className='video__player'
-      loop
-      src="https://www.w3schools.com/html/mov_bbb.mp4"></video>
+      <video
+        onClick={handleVideoPress}
+        className="video__player"
+        loop
+        ref={videoRef}
+        src="https://www.w3schools.com/html/mov_bbb.mp4"
+      ></video>
       {/* VideoFooter */}
       {/* VideoSidebar */}
     </div>
