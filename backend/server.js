d3e597da5d1df8cecb50aa756eb90af0c32deacb
D-Limiter <>
2024-07-29 19:49:43 +0800

    Finalise backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const Data = require("./data.js");
+// const
 import Data from "./data.js";
 import Videos from "./dbModel.js";
 
 //app config
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 
 //middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Headers", "*"),
+    next();
+});
 
 //DB Config
 const connection_url =
@@ -21,6 +31,16 @@ mongoose.connect(connection_url, {
 app.get("/", (req, res) => res.status(200).send("hello world"));
 
 app.get("/v1/posts", (req, res) => res.status(200).json(Data));
+
+app.get("/v2/posts", (req, res) => {
+  Videos.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 app.post("v2/posts", (req, res) => {
   const dbVideos = req.body;
